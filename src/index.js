abb7829de45083891a8b4e50bbcd4f7341ca39fa
Ekaterina <e.moskovkina@wwpass.com>
2020-06-11 14:07:38 +0300

    fixing package.json and package structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import * as asn1js from 'asn1js';
 import { version } from '../package.json';
 import {
   generatePrivateKeyAndCSR,
@@ -15,46 +14,8 @@ import {
   EXTN_ID,
   CRITICAL,
   EXTN_VALUE
-} from "./generatePEMs"
+} from './generatePEMs'
 
 if ('console' in window && window.console.log) {
   window.console.log(`WWPass frontend library version ${version}`);
-  window.console.log('Integer', asn1js.Integer);
-  window.console.log(asn1js);
-  let companyData = {
-    [OID_COMMON_NAME]: "*.wikipedia.org",
-    [OID_ORGANIZATION_NAME]: "Wikimedia Foundation, Inc.",
-    [OID_ORGANIZATIONAL_UNIT_NAME]: "Finance",
-    [OID_LOCALITY_NAME]: "San Francisco",
-    [OID_STATE_OR_PROVINCE_NAME]: "California",
-    [OID_COUNTRY_NAME]: "US",
-    [OID_EMAIL_ADDRESS]: "none.none@wikipedia.org",
-    [OID_EXTENSION_REQUEST]: [
-      {
-        [EXTN_ID]: "2.5.29.36",
-        [CRITICAL]: false,
-        [EXTN_VALUE]: new Uint8Array([48, 6, 2, 1, 1, 2, 1, 2]).buffer
-      },
-      {
-        [EXTN_ID]: "2.5.29.19",
-        [CRITICAL]: false,
-        [EXTN_VALUE]: new Uint8Array([48, 6, 1, 1, 0, 2, 1, 0]).buffer
-      }
-    ],
-    [OID_CHALLENGE_PASSWORD]: "qwerty",
-    [OID_UNSTRUCTURED_NAME]: "Some Name"
-  };
-  generatePrivateKeyAndCSR(companyData).then(result => {
-    addToPage(result.CSR_PEM);
-    addToPage(result.privateKeyPEM);
-  });
-}
-
-function addToPage(text){
-  var outer = document.createElement('div');
-  var inner = document.createElement('code');
-  inner.style.cssText = 'white-space: pre-wrap;';
-  inner.textContent = text;
-  outer.appendChild(inner);
-  document.body.appendChild(outer);
 }
