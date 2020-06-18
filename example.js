ef5883757c6aa2e348d8aeee3c2ac7b3c394fbde
Ekaterina <e.moskovkina@wwpass.com>
2020-06-18 23:16:20 +0300

    add example usage

---
+++ example.js
@@ -0,0 +1,36 @@
+function addToPage(text){
+  var outer = document.createElement('div');
+  var inner = document.createElement('code');
+  inner.style.cssText = 'white-space: pre-wrap;';
+  inner.textContent = text;
+  outer.appendChild(inner);
+  document.body.appendChild(outer);
+}
+
+const companyData = {
+  [PEM.OID.COMMON_NAME]: "*.wikipedia.org",
+  [PEM.OID.ORGANIZATION_NAME]: "Wikimedia Foundation, Inc.",
+  [PEM.OID.ORGANIZATIONAL_UNIT_NAME]: "Finance",
+  [PEM.OID.LOCALITY_NAME]: "San Francisco",
+  [PEM.OID.STATE_OR_PROVINCE_NAME]: "California",
+  [PEM.OID.COUNTRY_NAME]: "US",
+  [PEM.OID.EMAIL_ADDRESS]: "none.none@wikipedia.org",
+//  [PEM.OID.EXTENSION_REQUEST]: [
+//    {
+//      [PEM.EXTN.ID]: "2.5.29.36",
+//      [PEM.EXTN.CRITICAL]: false,
+//      [PEM.EXTN.VALUE]: new Uint8Array([48, 6, 2, 1, 1, 2, 1, 2]).buffer
+//    },
+//    {
+//      [PEM.EXTN.ID]: "2.5.29.19",
+//      [PEM.EXTN.CRITICAL]: false,
+//      [PEM.EXTN.VALUE]: new Uint8Array([48, 6, 1, 1, 0, 2, 1, 0]).buffer
+//    }
+//  ],
+//  [PEM.OID.CHALLENGE_PASSWORD]: "qwerty",
+//  [PEM.OID.UNSTRUCTURED_NAME]: "Some Name"
+};
+PEM.generate(companyData).then(result => {
+  addToPage(result.CSR_PEM);
+  addToPage(result.privateKeyPEM);
+});
