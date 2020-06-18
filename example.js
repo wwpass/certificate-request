ca2c17f701962f92f5dfe13f3edf82fc2a692f50
Ekaterina <e.moskovkina@wwpass.com>
2020-06-18 23:27:16 +0300

    fix in example

--- example.js
+++ example.js
@@ -15,20 +15,20 @@ const companyData = {
   [PEM.OID.STATE_OR_PROVINCE_NAME]: "California",
   [PEM.OID.COUNTRY_NAME]: "US",
   [PEM.OID.EMAIL_ADDRESS]: "none.none@wikipedia.org",
-//  [PEM.OID.EXTENSION_REQUEST]: [
-//    {
-//      [PEM.EXTN.ID]: "2.5.29.36",
-//      [PEM.EXTN.CRITICAL]: false,
-//      [PEM.EXTN.VALUE]: new Uint8Array([48, 6, 2, 1, 1, 2, 1, 2]).buffer
-//    },
-//    {
-//      [PEM.EXTN.ID]: "2.5.29.19",
-//      [PEM.EXTN.CRITICAL]: false,
-//      [PEM.EXTN.VALUE]: new Uint8Array([48, 6, 1, 1, 0, 2, 1, 0]).buffer
-//    }
-//  ],
-//  [PEM.OID.CHALLENGE_PASSWORD]: "qwerty",
-//  [PEM.OID.UNSTRUCTURED_NAME]: "Some Name"
+  [PEM.OID.EXTENSION_REQUEST]: [
+    {
+      [PEM.EXTN.ID]: "2.5.29.36",
+      [PEM.EXTN.CRITICAL]: false,
+      [PEM.EXTN.VALUE]: new Uint8Array([48, 6, 2, 1, 1, 2, 1, 2]).buffer
+    },
+    {
+      [PEM.EXTN.ID]: "2.5.29.19",
+      [PEM.EXTN.CRITICAL]: false,
+      [PEM.EXTN.VALUE]: new Uint8Array([48, 6, 1, 1, 0, 2, 1, 0]).buffer
+    }
+  ],
+  [PEM.OID.CHALLENGE_PASSWORD]: "qwerty",
+  [PEM.OID.UNSTRUCTURED_NAME]: "Some Name"
 };
 PEM.generate(companyData).then(result => {
   addToPage(result.CSR_PEM);
